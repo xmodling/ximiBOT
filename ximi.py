d79c5e1c8e3a9e5a2cccff2a2b0c052b7c367d95
xmodling <70622770+xmodling@users.noreply.github.com>
2021-02-09 00:39:39 +0300

    Update ximi.py

--- ximi.py
+++ ximi.py
@@ -19,9 +19,9 @@ for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             if '.report' in event.object.text.lower():
                 if len(event.object.text.lower()) > 15:
-                    vk.method("messages.send", {"peer_id":event.object['peer_id'], "Сообщение отправлено. ✅":donate, "random_id": random.randint(1, 2132138123)})
+                    vk.method("messages.send", {"peer_id":event.object['peer_id'], "message": "Сообщение отправлено. ✅", "random_id": random.randint(1, 2132138123)})
                 else:
-                    vk.method("messages.send", {"peer_id":event.object['peer_id'], "Введите более подробное описание. ❌":donate, "random_id": random.randint(1, 2132138123)})
+                    vk.method("messages.send", {"peer_id":event.object['peer_id'], "message": "Введите более подробное описание. ❌", "random_id": random.randint(1, 2132138123)})
             if event.object.text.lower() == '.help':
                 vk.method("messages.send", {"peer_id":event.object['peer_id'], "attachment": "photo-201383746_457239022", "message":cmdlist, "random_id": random.randint(1, 2132138123)})    
             elif event.object.text.lower() == '.donate':
